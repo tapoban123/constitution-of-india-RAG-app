657ab7e9f56aa8fc87bbf585058fcb1905941954
Tapoban Ray <raytapoban@gmail.com>
2025-08-30 02:10:41 +0530

    Added langchain-pinecone as directory and updated requirements.txt

--- back_end/api/utils/pinecone_db_mng.py
+++ back_end/api/utils/pinecone_db_mng.py
@@ -1,6 +1,6 @@
 from langchain_core.documents import Document
 from langchain_pinecone import PineconeVectorStore
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 from utils.constants import PINECONE
 from pinecone import Pinecone, CloudProvider, AwsRegion, Metric, IndexEmbed, EmbedModel, ServerlessSpec
